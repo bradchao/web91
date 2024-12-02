d5d75cf1315ff8bba1491946ff48a01322667073
User <User@B01.r1805.tciii.org.tw>
2024-12-02 10:51:54 +0800

    12021051

---
+++ BradWeb/src/main/java/tw/brad/tags/HelloTag.java
@@ -0,0 +1,38 @@
+package tw.brad.tags;
+
+import java.io.IOException;
+
+import jakarta.servlet.jsp.JspContext;
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.tagext.JspFragment;
+import jakarta.servlet.jsp.tagext.SimpleTagSupport;
+
+public class HelloTag extends SimpleTagSupport {
+	private String name;
+	
+	public void setPname(String pname) {
+		this.name = name;
+	}
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		JspContext jsp = getJspContext();
+		JspWriter out = jsp.getOut();
+
+		if (name != null) {
+			out.print(String.format("Hello, %s", name));
+		}else {
+			out.print(String.format("Hello, %s", "World"));
+		}
+		
+		JspFragment fragment = getJspBody();
+		if (fragment != null) {
+			out.print("I am HelloTag JspFragment<br />");
+			fragment.invoke(out);
+		}
+		
+		
+	}
+
+}
