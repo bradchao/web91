9a8369e7224fd84f23d63f835dafd24d35ad8c87
User <User@B01.r1805.tciii.org.tw>
2024-12-03 15:46:40 +0800

    12031546

--- BradWeb/src/main/webapp/js/client1.js
+++ BradWeb/src/main/webapp/js/client1.js
@@ -1,9 +1,43 @@
 window.onload = function(){
-	var canvas, ctx;
+	var canvas, ctx, clear;
 	var isDrag = false;
+	var webSocket = new WebSocket("ws://10.0.104.177:8080/BradWeb/myserver");
+	var isConnect = false;
 	
+	webSocket.onopen = function(event){
+		isConnect = true;
+	};
+	webSocket.onmessage = function(event){
+		
+	}
+	webSocket.onclose = function(event){
+		isConnect = false;
+	}
+	webSocket.onerror = function(event){
+		
+	}
+	
+	
+	
+	clear = document.getElementById('clear');
 	canvas = document.getElementById('myDrawer');
 	ctx = canvas.getContext('2d');
+	
+	clear.addEventListener("click",function(event){
+		ctx.clearRect(0,0, canvas.width, canvas.height);
+		
+		if (isConnect){
+			var data = {
+				isClear: true
+			}
+
+			if (isConnect){
+				webSocket.send(JSON.stringify(data));
+			}
+			
+		}
+		
+	});
 	
 	canvas.onmousedown = function(event){
 		isDrag = true;
@@ -12,6 +46,18 @@ window.onload = function(){
 		ctx.lineWidth = 4;
 		ctx.moveTo(x, y);
 		console.log("down:" + x + ";" + y);
+		
+		var data = {
+			isClear: false,
+			isNewLine : true,
+			x : x,
+			y : y
+		}
+		
+		if (isConnect){
+			webSocket.send(JSON.stringify(data));
+		}
+		
 	};
 	
 	canvas.onmouseup = function(event){
@@ -24,6 +70,19 @@ window.onload = function(){
 			ctx.lineTo(x,y);
 			ctx.stroke();
 			console.log("move:" + x + ";" + y);
+			
+			var data = {
+				isClear : false,
+				isNewLine : false,
+				x: x,
+				y: y
+			};
+			
+			if (isConnect){
+				webSocket.send(JSON.stringify(data));
+			}
+			
+			
 		}
 	};
 	
